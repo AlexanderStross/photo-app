2626546f168b34ff4ef0e15134f5d35683a24e8e
“abstross” <abstross@pm.me>
2021-08-22 15:17:10 -0500

    Doing this in a modern way

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,5 @@
 class Payment < ApplicationRecord
+  require 'stripe'
   attr_accessor :card_number, :card_cvc, :card_expires_month, :card_expires_year
 
   belongs_to :user
@@ -12,11 +13,10 @@ class Payment < ApplicationRecord
   end
 
   def process_payment
-    customer = Stripe::Customer.create email: email, card: token
-    byebug
-    Stripe::Charge.create customer: customer.id,
-                          amount: 1000,
-                          description: 'Premium',
-                          currency: 'usd'
+    customer = Stripe::Customer.create({ email: email, card: token })
+    Stripe::Charge.create({ customer: customer.id,
+                            amount: 1000,
+                            description: 'Premium',
+                            currency: 'usd' })
   end
 end
