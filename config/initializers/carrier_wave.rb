60f8cf005f60a27845959ab86f7f90b542cb8973
“abstross” <abstross@pm.me>
2021-08-23 16:55:33 -0500

    meh

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,4 @@
-  Fog::Storage::AWS::DEFAULT_REGION = 'us-east-2
 if Rails.env.production?
-
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
@@ -8,5 +6,6 @@ if Rails.env.production?
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
+    config.fog_regioun = ENV['S3_REGION']
   end
 end
