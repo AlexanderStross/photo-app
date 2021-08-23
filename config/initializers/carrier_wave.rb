cc65bc8cfa66a9cc3d56f9efe34860e934afe7b1
“abstross” <abstross@pm.me>
2021-08-23 17:17:02 -0500

    s3 problems

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      region: 'us-east-2',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      host: 's3.amazonaws.com',
+      region: 'us-east-2'
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
